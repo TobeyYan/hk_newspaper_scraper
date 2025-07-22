a245af056bdef29070a87291a6d92dc074aaeb56
TobeyYan <hyan@oxbridge-econ.com>
2025-07-22 09:30:19 +0800

    Change start date to 2017

--- am730_scraper.py
+++ am730_scraper.py
@@ -23,7 +23,7 @@ logging.basicConfig(
 logger = logging.getLogger(__name__)
 
 # --- CONFIGURATION ---
-START_DATE = datetime(2015, 7, 14)
+START_DATE = datetime(2017, 2, 9)
 END_DATE = datetime(2025, 7, 18)
 PUBLISHER_NAME = "am730"
 TEMP_DIR = "temp_downloads"
