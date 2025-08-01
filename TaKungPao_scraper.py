0b0ec3aa0dd15fc3d50e81006b748c758e0dac10
TobeyYan <hyan@oxbridge-econ.com>
2025-08-01 09:52:05 +0800

    2024!

--- TaKungPao_scraper.py
+++ TaKungPao_scraper.py
@@ -30,8 +30,8 @@ logger = logging.getLogger(__name__)
 
 # Constants
 BASE_URL_FORMAT = "http://www.takungpao.com.hk/paper/{date_str}.html"
-START_DATE = datetime(2022, 7, 8) # This is your desired start date
-END_DATE = datetime(2023, 12, 31)
+START_DATE = datetime(2023, 11, 30) # This is your desired start date
+END_DATE = datetime(2024, 12, 31)
 PUBLISHER_NAME = "TaKungPao"
 TEMP_PDF_DIR = "temp_downloads"
 MISSING_PAGES_LOG = "missing_pages.log" # New file for missing pages
